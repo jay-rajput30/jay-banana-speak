ffae1693bce25300068f06644d9d7fab5ab9f502
Jay Rajput <rajputjay901@gmail.com>
2020-11-29 09:39:31 +0530

    updated fetch call

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const btn = document.querySelector(".btn");
 const txtArea = document.querySelector("#txtArea");
 const output = document.querySelector(".output");
 
-btn.addEventListener('click', ()=>{
- output.innerText = txtArea.value;
-})
+btn.addEventListener("click", () => {
+  let url =
+    "https://api.funtranslations.com/translate/minion.json?text=" +
+    txtArea.value;
+  // console.log(url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+     var translatedTxt =  data.contents.translated;
+     console.log(translatedTxt);
+    })
+    .catch((e) => console.log("error: " + e));
+});
